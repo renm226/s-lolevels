a7f5ccf25ffada6d39eb8361db0040ef8844c04d
renm226 <samsono.odwori@gmail.com>
2025-10-29 15:47:30 +0300

    Fetching user input

--- 0levelc/printformat.c
+++ 0levelc/printformat.c
@@ -8,5 +8,5 @@ int main(){
     //we can use fromat specifier to print variables so that we dont have to use multiple print statements
     //this is easy that calling variable or value in multiple print statements
     printf("The sum of %d, %d and %d is %d\n", i, j, k, l);
-    return 0;
+   return 0;
 }
