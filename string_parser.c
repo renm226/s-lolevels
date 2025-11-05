b1d2976f75f2cf2ca4742a2a16bc7de79b3c778b
renm226 <samsono.odwori@gmail.com>
2025-11-05 11:06:00 +0300

    added string parser function

---
+++ string_parser.c
@@ -0,0 +1,15 @@
+// string_parser.c
+#include <stdio.h>
+#include <string.h>
+
+void parse_string(const char *s) {
+    if (!s) { printf("null input\n"); return; }
+    for(size_t i=0;i<strlen(s);i++){
+        printf("char %zu: %c\n", i, s[i]);
+    }
+}
+
+int main(void) {
+    parse_string("hello");
+    return 0;
+}
