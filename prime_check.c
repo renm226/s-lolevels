6a9b6d1435cc36cf7a7e9be5014c85441074fbb8
renm226 <samsono.odwori@gmail.com>
2025-11-06 11:06:00 +0300

    added prime number checker

---
+++ prime_check.c
@@ -0,0 +1,19 @@
+// prime_check.c
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_prime(int n) {
+    if(n <= 1) return false;
+    for(int i=2;i*i<=n;i++)
+        if(n % i == 0) return false;
+    return true;
+}
+
+int main(void) {
+    int n = 17;
+    if(is_prime(n))
+        printf("%d is prime\n", n);
+    else
+        printf("%d is not prime\n", n);
+    return 0;
+}
