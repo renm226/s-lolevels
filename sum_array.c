86908bcc195e408f9437c64c339a2f2709535853
renm226 <samsono.odwori@gmail.com>
2025-11-06 11:12:00 +0300

    summed elements of an array

---
+++ sum_array.c
@@ -0,0 +1,11 @@
+// sum_array.c
+#include <stdio.h>
+
+int main(void) {
+    int arr[] = {1,2,3,4,5};
+    int sum = 0;
+    for(int i=0;i<5;i++)
+        sum += arr[i];
+    printf("Sum of array elements: %d\n", sum);
+    return 0;
+}
