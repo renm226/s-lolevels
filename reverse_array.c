c89066c470f225898a1c544360e4172691a1de91
renm226 <samsono.odwori@gmail.com>
2025-11-07 11:12:00 +0300

    reversed array elements

---
+++ reverse_array.c
@@ -0,0 +1,17 @@
+// reverse_array.c
+#include <stdio.h>
+
+int main(void) {
+    int arr[] = {1,2,3,4,5};
+    int n = 5;
+    for(int i=0;i<n/2;i++){
+        int temp = arr[i];
+        arr[i] = arr[n-1-i];
+        arr[n-1-i] = temp;
+    }
+    printf("Reversed array: ");
+    for(int i=0;i<n;i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
