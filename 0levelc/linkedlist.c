e7bfcef5e0a929d0a7d962715da5dc9aa0d7dc78
renm226 <samsono.odwori@gmail.com>
2025-11-19 10:00:00 +0300

    add a singlylinkedlist that usecase can be in :Dynamic size
    Fast insert/delete
    
    Poor cache locality

---
+++ 0levelc/linkedlist.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node *next;
+} Node;
+
+Node* list_insert_front(Node *head, int value) {
+    Node *n = malloc(sizeof(Node));
+    n->value = value;
+    n->next = head;
+    return n;
+}
+
+void list_print(Node *head) {
+    while (head) {
+        printf("%d -> ", head->value);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+void list_free(Node *head) {
+    while (head) {
+        Node *tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+}
+
+int main() {
+    Node *head = NULL;
+
+    head = list_insert_front(head, 10);
+    head = list_insert_front(head, 20);
+    head = list_insert_front(head, 30);
+
+    list_print(head);
+    list_free(head);
+    return 0;
+}
