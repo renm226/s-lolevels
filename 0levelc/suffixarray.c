0d10aed084d7543bf2aaff43fcaf5168dd974a42
renm226 <samsono.odwori@gmail.com>
2026-01-03 11:17:06 +0300

    Function to build suffix array

--- 0levelc/suffixarray.c
+++ 0levelc/suffixarray.c
@@ -1,1 +1,11 @@
 //Suffix Array (Simpler Alternative to Suffix Tree)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// Function to build suffix array
+int cmpfunc(const void *a, const void *b, void *str) {
+    const char *s = (const char *)str;
+    int i1 = *(int *)a;
+    int i2 = *(int *)b;
+    return strcmp(s + i1, s + i2);
+}
