9d88167649fc3e58478680b210c8910ff5606576
renm226 <samsono.odwori@gmail.com>
2026-01-04 11:26:06 +0300

    Function to insert a new element into the heap

--- 0levelc/heap.c
+++ 0levelc/heap.c
@@ -1,1 +1,20 @@
 //Binary Min-Heap (Array-Based)
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CAPACITY 50
+// Min-Heap structure
+typedef struct {
+    int data[CAPACITY];
+    int size;
+} MinHeap;
+
+void heap_init(MinHeap *h) {
+    h->size = 0;
+}
+// Function to insert a new element into the heap
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
